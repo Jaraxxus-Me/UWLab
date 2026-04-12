c13e8d0274708a1c47277715b495aeca80c7f384
Bowen Li <bowenli2@andrew.cmu.edu>
2026-04-11 21:12:25 -0400

    working now

--- test_pretrained_policy.sh
+++ test_pretrained_policy.sh
@@ -2,4 +2,5 @@ python demos/test_ur5e_hybrid_policy.py \
     --checkpoint cube_state_rl_expert_seed42.pt \
     --num_envs 1 --num_episodes 5 --max_steps 1000 \
     env.scene.insertive_object=cube \
-    env.scene.receptive_object=cube
+    env.scene.receptive_object=cube \
+    env.episode_length_s=100.0
