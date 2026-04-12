56d73443e909b6b128857d67acb1914306166b5a
Bowen Li <bowenli2@andrew.cmu.edu>
2026-04-11 20:15:25 -0400

    added policy for sanity check

--- test_pretrained_policy.sh
+++ test_pretrained_policy.sh
@@ -1,5 +1,5 @@
 python demos/test_ur5e_hybrid_policy.py \
-    --checkpoint rectangle_state_rl_expert_seed0.pt \
-    --num_envs 1 --num_episodes 5 --max_steps 400 \
-    env.scene.insertive_object=rectangle \
-    env.scene.receptive_object=wall
+    --checkpoint cube_state_rl_expert_seed42.pt \
+    --num_envs 1 --num_episodes 5 --max_steps 1000 \
+    env.scene.insertive_object=cube \
+    env.scene.receptive_object=cube
