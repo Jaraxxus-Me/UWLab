c5f0ad31b5bbb6faeaa40669a3eae2c836071dfb
Bowen Li <bowenli2@andrew.cmu.edu>
2026-04-24 10:52:02 -0400

    ready to start for pre-trained

--- scripts_v2/train_from_pretrained.sh
+++ scripts_v2/train_from_pretrained.sh
@@ -3,6 +3,7 @@ python scripts/reinforcement_learning/rsl_rl/train.py \
     --num_envs 8192 \
     --logger wandb \
     --headless \
+    --resume_path cube_state_rl_expert_seed42.pt \
     env.scene.insertive_object=block \
     env.scene.receptive_object=box \
     env.events.reset_from_reset_states.params.dataset_dir=./Datasets/OmniReset
